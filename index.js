c2e642620b892f4ef4045de0ccffc471f7cfeed9
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-11-13 15:55:36 -0800

    updated the template function.. working properly now

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
 });
 
 const STORE = {
-  thumbnails: [],
-  videoURLS: []
+  data: []
 };
 
 const apiEndpoint = 'https://www.googleapis.com/youtube/v3/search';
@@ -24,7 +23,6 @@ function queryAPI(query) {
   let url = apiEndpoint;
   $.getJSON(url, params, function(data) {
     showResults(data.items);
-    console.log(data);
   });
 }
 
@@ -41,36 +39,22 @@ function getSearchTerms() {
 }
 
 // *** Template Generators
-// function template(){
-//   console.log(STORE.videoURLS);
-//   const urlValue = STORE.videoURLS.map(function(value){
-//     return `<li><a href="${value}">`;});
-//   const thumbnailValue = STORE.thumbnails.map(function(value){
-//     return `<img src="${value}" alt="thumbnail text here"></a></li>`;});
-//   return `<ul>${urlValue}${thumbnailValue}</ul>`;
-// }
-
 function template() {
-  STORE.thumbnails.map(thumbnail => {
-    STORE.videoURLS.map(video => {
-      return (
-        `<li><a href="${video}"><img src="${thumbnail}" alt="thumbnail text here"></a></li>`
-      );
-    });
+  console.log(STORE.data);
+  STORE.data.forEach(item => {
+    $('.thumbnails').append(`<li><a href="${item.videoURLS}"><img src="${item.thumbnail}" alt="thumbnail text here"></a></li>`);
   });
-  generateHTML();
-}
-
-function generateHTML() {
-  $('.thumbnails').html(template());
 }
 
 // *** Renderers
 function showResults(result){
   result.forEach(function(value, index){
-    STORE.thumbnails.push(value.snippet.thumbnails.default.url);
-    STORE.videoURLS.push('https://www.youtube.com/watch?v='+value.id.videoId)
+    const obj = {
+      thumbnail: value.snippet.thumbnails.default.url,
+      videoURLS: 'https://www.youtube.com/watch?v='+value.id.videoId
+    }
+    STORE.data.push(obj);
   });
-  generateHTML();
+  template();
 }
 
